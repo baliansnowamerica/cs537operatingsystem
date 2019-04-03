f25b91c34be7c45f6f8ea2165c4e225a4d938ea5
baliansnowamerica <baliansnowamerica@gmail.com>
2019-04-03 04:03:06 -0500

    all tests passed without thread free

--- p4a/mapreduce.c
+++ p4a/mapreduce.c
@@ -1,28 +1,151 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <pthread.h>
 #include "mapreduce.h"
 
-// a value linkedlist
+// linkedlist of values, and each value has its own data 
+// one key could be corresponding to many values in a pair
 typedef struct node_t {
-    char *value;
-    struct node_t *next;
-} values;
+    char *data;
+    struct node_t *nextv;
+} value;
 
 // global getter current value
-values *gettercv = NULL;
+// gettercv is used to point to address of a "variable value" in the linkedlist
+value *gettercv = NULL;
 
-// a pair starts with a key and a value
-// but value can keep growing for a pair
+// a pair starts with a key and its corresponding value
+// the corresponding value is initialized as head of a linked list
+// future values that corresponding to the key will be added to the linked list
 typedef struct dict_t {
     char *key;
-    int sv; // size of values
-    values *value;
+    value *values; // head of the linked list
+    int sv;        // size of values, which is size of the linked list
+    struct dict_t *nextp; // different keys may have the same hashcode, so use chainedbuckets to solve this collision
 } pair;
 
-pair mapping[11000];
-int pairsize = 0;
+/**
+ * This function inserts new value into linked list of values for a key-value pair.
+ */
+void insertpairvalue(pair *curpair, char *data) {
+    // create a new variable value for data, which is corresponding to curpair
+    // note. (*newvalue).value is equal to newvalue->value 
+    value *newvalue = malloc(sizeof(value));
+    newvalue->data = malloc(sizeof(char)*(strlen(data)+1));
+    strcpy((*newvalue).data, data);
+    newvalue->nextv = curpair->values;
+    // insert value newvalue into the head of linked list of curpair
+    curpair->values = newvalue;
+    curpair->sv++;
+}
+
+// hashmap for storing key-values pairs
+// each index of a hashmap is a linked list that composed of pairs
+typedef struct {
+    pair **hasharray;   // hashArray with chainedbuckets, each chainedbuckets has an index in array
+    int chainedbuckets; // number of chainedBuckets, which is size of the array, that can be used in this array
+    int numcurpair;     // currently, number of paris (HashNodes) in this hash array 
+    double loadFactor;  // threshold of expanding and rehashing
+} pairhashmap;
+
+// global data structure for storing pairs
+pairhashmap *mapping;
+
+/**
+ * This function inserts a new pair into the end of a "pairs linked list".
+ */
+void insertpairpair(pairhashmap *map, pair *newpair, int index) {
+    // printf("key: %s, value: %s", curpair->key, curpair->values->data);
+    pair *head = map->hasharray[index];
+    map->hasharray[index] = newpair;
+    newpair->nextp = head;
+}
+
+/**
+ * This function initialize a hashmap
+ */
+void initpairhashmap(pairhashmap *map, int initialsize) {
+    map->hasharray = malloc(sizeof(pair *)*initialsize); // memory size of this hashmap
+    for (int i=0; i<initialsize; i++) {
+        map->hasharray[i] = NULL;
+    }
+    map->chainedbuckets = initialsize; // number of indices for this hashmap
+    map->numcurpair = 0; // number of pairs that are in this hashmap
+    map->loadFactor = 0.7; // increase size of this hashmap when numcurpair/chainedbuckets >= 0.7
+}
+
+/**
+ * This function return the index of a key for a hashmap
+ */
+int getindexhashmap(char *key, int size) {
+    unsigned long int hash = 0;
+    unsigned int i = 0;
+    // convert string to int
+    while(i < strlen(key)){
+        hash = hash << 8;
+        hash += key[i];
+        i++;
+    }
+
+    return hash % size;
+}
+
+/**
+ * This function returns an existed pair that corresponds to a given key,
+ * otherwise, return NULL.
+ */
+pair *findpairhashmap(pairhashmap *map, char *key, int index) {
+    pair *temppair = map->hasharray[index];
+    while (temppair != NULL) {
+        if (strcmp(temppair->key, key) == 0)
+            return temppair;
+        temppair = temppair->nextp;
+    }
+    return NULL;
+}
+
+/**
+ * This functino inserts a new pair into a hashmap.
+ */
+void insertpairhashmap(pairhashmap *map, pair *newpair, int index) {
+    // insert new pair to the pair linkedlist
+    // printf("index: %d\n", index);
+    insertpairpair(map, newpair, index);
+    map->numcurpair ++;
+    // printf("map->hasharray[index]->key: %s", map->hasharray[index]->key);
+
+    // increase hashmap when exceed loadfactor, new chainedBucket = 2*chainedBucket-1
+    if (map->numcurpair / map->chainedbuckets >= map->loadFactor) {
+        // create a new hasharray
+        pair **newhasharray = malloc(sizeof(pair *)*(map->chainedbuckets*2-1));
+        for (int i=0; i<map->chainedbuckets*2-1; i++) {
+            newhasharray[i] = NULL;
+        }
+        // put each pair of mapping into newmapping
+        map->numcurpair = 0;
+        int oldchainedbuckets = map->chainedbuckets;
+        map->chainedbuckets = map->chainedbuckets*2-1;
+        pair **oldhasharray = map->hasharray; // *
+        map->hasharray = newhasharray; // &
+        for (int i=0; i<oldchainedbuckets; i++) {
+            // get new index for each existed pair in mapping
+            if (oldhasharray[i] != NULL) {
+                pair *temppair = oldhasharray[i];
+                while (temppair != NULL) {
+                    index = getindexhashmap(temppair->key, map->chainedbuckets);
+                    pair *nextpair = temppair->nextp;
+                    temppair->nextp = NULL;
+                    insertpairhashmap(map, temppair, index);
+                    temppair = nextpair;
+                }
+                // printf("key: %s, value: %s", map->hasharray[i]->key, map->hasharray[i]->values->data);
+            }
+        } 
+        free(oldhasharray);
+    }
+}
 
 /**
  * MR_Emit recevies a key-value pair, and stores this pair in a data structure.
@@ -30,69 +153,74 @@ int pairsize = 0;
  * 
  * The meaning of key-value pair could be any.
  * One of a possible meaning could be "a specific key has occured value times".
- * 
- * @param key is an address of a string
- * @param value is an address of a string
  */
 void MR_Emit(char *key, char *value) {
-    for (int i=0; i<=pairsize; i++) {
-        // when a specific key doesn't exist, create it in mapping data structure 
-        if (i == pairsize) {
-            // size of values in this pair
-            mapping[i].sv ++;
-            // key
-            // char *check = malloc(sizeof(char)*(strlen(key)+1));
-            // printf("lala check: %s\n", check);
-            mapping[i].key = malloc(sizeof(char)*(strlen(key)+1));
-            strcpy(mapping[i].key, key);
-
-            // value of values, (*newvalues).value is equal to newvalues->value 
-            values *newvalues = malloc(sizeof(values));
-            newvalues->value = malloc(sizeof(char)*(strlen(value)+1));
-            strcpy((*newvalues).value, value);
-            // next of values
-            newvalues->next = NULL;
-            // all for values
-            mapping[i].value = newvalues;
-            pairsize ++;
-            break;
-        }
-        // when a specific key exist, updates its value 
-        else if (strcmp(mapping[i].key, key) == 0) {
-            // size of values in this pair
-            mapping[i].sv ++;
-
-            // value of values, (*newvalues).value is equal to newvalues->value 
-            values *newvalues = malloc(sizeof(values));
-            newvalues->value = malloc(sizeof(char)*(strlen(value)+1));
-            strcpy((*newvalues).value, value);
-            // next of values
-            newvalues->next = mapping[i].value;
-            // all for values
-            mapping[i].value = newvalues;
-
-            break;
-        }
+    // this function returns the index of a key for a hashmap
+    int index = getindexhashmap(key, mapping->chainedbuckets);
+    // this function returns an existed pair that corresponds to a given key in a hashmap, otherwise, return NULL
+    pair *existedpair = findpairhashmap(mapping, key, index);
+    // existedpair doesn't exist, so create a new pair and insert it to mapping
+    if (existedpair == NULL) {
+        // printf("NULL key: %s, value: %s, index: %d, mapping->numcurpair: %d.\n", key, value, index, mapping->numcurpair);
+        // create a new pair
+        pair *newpair = malloc(sizeof(pair)); // initialize memory
+        newpair->key = malloc(sizeof(char)*(strlen(key)+1));
+        newpair->values = NULL;
+        newpair->nextp = NULL;
+        strcpy(newpair->key, key); // set key
+        insertpairvalue(newpair, value); // set values
+        newpair->sv = 1; // set size of values, which is a linked list 
+        // printf("key: %s, value: %s, sv: %d\n", newpair->key, newpair->values->data, newpair->sv);
+        // this functino inserts a new pair into a hashmap
+        insertpairhashmap(mapping, newpair, index);
+    }
+    // existedpair exists, so update its values
+    else {
+        // printf("NON-NULL key: %s, value: %s, index: %d, mapping->numcurpair: %d.\n", key, value, index, mapping->numcurpair);
+        // this function inserts new value into linked list of values for a key-value pair
+        insertpairvalue(existedpair, value);
     }
 }
+
+/**
+ * typedef char *(*Getter)(char *key, int partition_number);
+ */
+char *get(char *key, int partition_number) {
+    // gettercv is used to point to address of a "variable value" in the linkedlist
+    // gettercv != NULL means that we are already pointing to a value in a linkedlist
+    if (gettercv != NULL) {
+        gettercv = gettercv->nextv;
+        return gettercv == NULL? NULL:gettercv->data;
+    } 
+
+    // get the index of matching pair in hashmap
+    int index = getindexhashmap(key, mapping->chainedbuckets);
+    // returns an existed pair that corresponds to a given key in a hashmap, otherwise, return NULL
+    pair *existedpair = findpairhashmap(mapping, key, index);
+    // there is no matching pair, return NULL
+    if (existedpair == NULL) return NULL;
+    else {
+        gettercv = existedpair->values;
+        return gettercv->data;
+    }
+}
+
+// function variable
+Getter mygetter = get;
 
 /**
  * Comparator for data type pair
  */
 int pair_comparator(const void *p, const void *q) { 
-    return strcmp(((pair *) p)->key, ((pair *) q)->key);
+    return strcmp((*(pair **) p)->key, (*(pair **) q)->key);
 } 
 
 /**
- * 
+ * Comparator for values in a pair
  */
 int value_comparator(const void *p, const void *q) {
-    return strcmp((char *) p, (char *) q);
+    return strcmp(*(char **) p, *(char **) q);
 }
-
-
-
-
 
 /**
  * The function takes a given key and map it to a number, from 0 to num_partitions - 1.
@@ -109,155 +237,131 @@ unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
     return hash % num_partitions;
 }
 
-// typedef char *(*Getter)(char *key, int partition_number);
-char *get(char *key, int partition_number) {
-    if (gettercv != NULL) {
-        gettercv = gettercv->next;
-        return gettercv == NULL? NULL:gettercv->value;
-    } 
-
-    // traverse all partitions, use 1 to replace partition_number temporarily
-    // for each partition, traverse all pairs that we have within the partition
-    for (int i=0; i<pairsize; i++) {
-        // to check which pair is the pair that with pair.key = key
-        if (strcmp(key, mapping[i].key) != 0) continue;
-        // getter current value
-        gettercv = mapping[i].value;
-        return gettercv->value;
-    }
-    
-
-    return NULL;
-}
-Getter mygetter = get;
-
-
-//char *p = NULL; // create a pointer and assign it the address of buffer
-// if (strcmp(mapping[i].key, key) == 0) {
-                // printf("%s vs %s", mapping[i].key, key);
-                // char buffer[8]; // create a place to store text
-                // *p = buffer;
-
-                // // if (p == NULL) printf("say ya\n");
-            //     p = (char*) malloc(sizeof(int));
-            //     // strcpy(buffer, "1");
-            //     sprintf((void*) p, "%d", mapping[i].value); // create textual representation of 'i' 
-            //     printf("wtf\n");
-            //     return p;
-            // }
-
 /**
  * 
  */
 void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce, int num_reducers, Partitioner partition) {
-    pairsize = 0;
-    // pthread_t p[num_mappers];
+    // initialize a hashmap with size = 37
+    mapping = malloc(sizeof(pairhashmap));
+    initpairhashmap(mapping, 37);
     
-    // for (int i=0; i<num_mappers; i++) {
-    //     pthread_create(&p[i], NULL, mythread, &num_mappers);
-    //     pthread_join(p[i], NULL);
-    // }
-        
-    // printf("argc: %d\n", argc);
-    // for (int i=0; i<argc; i++) printf("argv[%d]: %s\n", i, argv[i]);
-    // printf("num_mappers: %d\n", num_mappers);
-    // printf("num_reducers: %d\n", num_reducers);
-    
+    // call map to update mapping
     for (int i=1; i<argc; i++) map(argv[i]);
+    printf("map done\n");
 
-    // printf("before sort values\n");
+    // sort values for each pair in the hashmap
+    for (int i=0; i<mapping->chainedbuckets; i++) {
+        // check whether this chainedbucket has a pairs linkedlist or not
+        if (mapping->hasharray[i] == NULL) continue;
+        // traverse all pairs in this pairs linkedlist
+        pair *temppair = mapping->hasharray[i];
+        // printf("i: %d, temppair->key: %s, temppair->values->data: %s\n", i, temppair->key, temppair->values->data);
+        while (temppair != NULL) {
+            // create array for sorting
+            char **temparray = malloc(sizeof(char *)*temppair->sv);
+            // update array
+            value *tempvalue = temppair->values;
+            int index = 0;
+            while (tempvalue != NULL) {
+                // printf("tempvalue->data: %s", tempvalue->data);
+                temparray[index] = tempvalue->data;
+                tempvalue = tempvalue->nextv;
+                index ++;
+            }
+            // sorting array
+            qsort(temparray, index, sizeof(char *), value_comparator);
+            // update values linkedlist of current pair
+            tempvalue = temppair->values;
+            for (int j=0; j<index; j++) {
+                tempvalue->data = temparray[j];
+                tempvalue = tempvalue->nextv;
+            }
 
-    // sort values of each key
-    for (int i=0; i<pairsize; i++) {
-        // create array for sorting
-        char **temparray = malloc(sizeof(char *)*mapping[i].sv);
-        int index = 0;
-        values *temp = mapping[i].value;
-        while (temp != NULL) {
-            temparray[index] = temp->value;
-            temp = temp->next;
-            index ++;
+            free(temparray);
+            temppair = temppair->nextp;
         }
-        // sorting array
-        qsort(temparray, index, sizeof(char*), value_comparator);
-        // update values linkedlist
-        temp = mapping[i].value;
-        
-        for (int j=0; j<index; j++) {
-            if (temp == NULL)
-            temp->value = temparray[j];
-            temp = temp->next;
-        }
-        free(temparray);
     }
+    printf("values sort done\n");
 
-    // printf("before qsort\n");
+    // sort pairs by key
+    // create an array for storing pairs in ascending order, and array index
+    pair **pairorder = malloc(sizeof(pair *)*mapping->numcurpair);
+    int ipairorder = 0;
+    for (int i=0; i<mapping->chainedbuckets; i++) {
+        // check whether this chainedbucket has a pairs linkedlist or not
+        if (mapping->hasharray[i] == NULL) continue;
+        // traverse all pairs in this pairs linkedlist
+        pair *temppair = mapping->hasharray[i];
+        while (temppair != NULL) {
+            // update keyorder array
+            pairorder[ipairorder] = temppair;
+            temppair = temppair->nextp;
+            ipairorder ++;
+        }
+        // printf("chainedbucket: %d\n", i);
+    }
+    qsort(pairorder, ipairorder, sizeof(pair *), pair_comparator);
+    printf("keys sort done, ipairorder: %d\n", ipairorder);
 
-    // sort key
-    qsort(&mapping, pairsize, sizeof(pair), pair_comparator);
-
-
-    // printf("pairsize: %d\n", pairsize);
-
-    // values *check = mapping[400].value;
-    // while (check->next != NULL) {
-    //     printf("%s\n", check->value);
-    //     check = check->next;
-    // }
-    
-    // printf("after qsort\n");
-
-    // creat an array for each partition, which is thread for reducer
-    // tp = threads pairs, which means that we store some pairs in a thread, and each thread has its own pair set
-    pair *tp[num_reducers][11000];
-    
-    // size of pairs of a thread, which means the number of pairs for each thread 
+    // size of pairs for a "reduce thread", which means the number of pairs for a "partition"
     int stp[num_reducers];
     for (int i=0; i<num_reducers; i++) stp[i] = 0; // initialize stp[i] to 0
-    
-    // update array of each partition, an array stores the pairs that need to be processed by the corresponding partition
-    for (int i=0; i<pairsize; i++) {
-        unsigned long index = partition(mapping[i].key, num_reducers);
-        tp[index][stp[index]] = &mapping[i];
+
+    // get the acutal size for each partition
+    for (int i=0; i<ipairorder; i++) {
+        unsigned long index = partition(pairorder[i]->key, num_reducers);
         stp[index] ++;
     }
+    printf("get partition size done\n");
+
+    // creat an pairs array for each partition, which can be considered as reduce thread or reducer
+    // pt = pairs of a thread, this means that we store some pairs in a thread, and each thread has its own pairs set
+    pair **pt[num_reducers];
+    for (int i=0; i<num_reducers; i++) {
+        pt[i] = malloc(sizeof(pair *)*stp[i]);
+    } 
+    
+    // update array of each partition, an array stores the pairs that need to be processed by the corresponding partition
+    for (int i=0; i<num_reducers; i++) stp[i] = 0; // re-initialize stp[i] to 0
+    for (int i=0; i<ipairorder; i++) {
+        unsigned long index = partition(pairorder[i]->key, num_reducers);
+        pt[index][stp[index]] = pairorder[i];
+        stp[index] ++;
+    }
+    printf("update partition done\n");
 
     // Reduce(char *key, Getter get_next, int partition_number)
     for (int i=0; i<num_reducers; i++) {
         for (int j=0; j<stp[i]; j++) {
             gettercv = NULL;
-            reduce(tp[i][j]->key, mygetter, i);
+            reduce(pt[i][j]->key, mygetter, i);
         }
     }
+    printf("reduce done\n");
     
-    
-    for (int i=0; i<pairsize; i++) {
-        // free key
-        free(mapping[i].key);
-        // free values
-        values *temp = mapping[i].value;
-        while (temp != NULL) {
-            values *forfree = temp->next;
-            free(temp->value);
-            free(temp);
-            temp = forfree;
-        }
-    }
 
+    // free pairs and their values
+    // for (int i=0; i<ipairorder; i++) {
+    //     // free value linkedlist for each pair
+    //     value *tempvalue = pairorder[i]->values;
+    //     while (tempvalue != NULL) {
+    //         value *forfree = tempvalue->nextv;
+    //         free(tempvalue->data);
+    //         free(tempvalue->nextv);
+    //         free(tempvalue);
+    //         tempvalue = forfree;
+    //     }
+    //     // free pair
+    //     free(pairorder[i]->key);
+    //     free(pairorder[i]->nextp);
+    // }
+
+    // // free partitions
+    // for (int i=0; i<num_reducers; i++) {
+    //     free(pt[i]);
+    // } 
+
+    // // free mapping
+    // free(mapping);
     // end of MR_Run
 }
-
-
-
-
-
-
-
-
-// /**
-//  * 
-//  */
-// void *mythread(void* arg) {
-//     printf("num_mappers: %d\n", *((int*) arg));
-//     return arg;
-// }
